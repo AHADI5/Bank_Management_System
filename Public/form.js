a5c29a26f741b191ce979311ce12a7880898cdf1
AHADI5 <gloireahadi9@gmail.com>
2022-12-27 14:18:49 +0200

    test for static elements front end

---
+++ Public/form.js
@@ -0,0 +1,15 @@
+var adminInput=document.getElementById('name');
+var formname =document.getElementById('labelname');
+function label(){
+    adminInput.placeholder='';
+    formname.style.display='block';
+    formname.style.fontSize='10px';
+    formname.style.fontWeight='bold';
+   
+    
+}
+
+
+
+
+
